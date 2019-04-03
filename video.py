8e04b84f301e7dc7e2a2c42441f9983e4156d21a
GewoonMarco <49206063+GewoonMarco@users.noreply.github.com>
2019-04-03 20:02:31 +0200

    Update video.py

--- video.py
+++ video.py
@@ -3,6 +3,10 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
  
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+ 
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name='trekvogels', type = 2))
@@ -17,12 +21,12 @@ async def on_member_join(member):
     await client.send_message(member, "-------------------------------------------------------------------")
    
    
-    @client.event
-     if message.content.startswith('!janee'):
+@client.event
+    if message.content.startswith('!janee'):
         randomlist = ['Ja','Nee']
         await client.send_message(message.channel,(random.choice(randomlist)))
-     if message.content.startswith('!vriend'):
+    if message.content.startswith('!vriend'):
         randomlist = ['20%','30%','40%','50%','60%','70%','80%','90%','100%']
         await client.send_message(message.channel,(random.choice(randomlist)))
        
- client.run('NTYyNjc4Mzk3MTUxNjA4ODM0.XKTi8Q.eXADJ1QD76WrMds6EHcyU3CTtLs')
+client.run('NTYyNjc4Mzk3MTUxNjA4ODM0.XKTi8Q.eXADJ1QD76WrMds6EHcyU3CTtLs')
