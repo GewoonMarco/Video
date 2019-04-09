fcda4a0ff1c0603777eb76da04e4d6e1f88b1141
GewoonMarco <49206063+GewoonMarco@users.noreply.github.com>
2019-04-09 20:40:59 +0200

    Update video.py

--- video.py
+++ video.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext.commands import Bot
 from discord.ext import commands
 import asyncio
+from discord import Game
  
 Client = discord.client
 client = commands.Bot(command_prefix = '!')
@@ -9,7 +10,7 @@ Clientdiscord = discord.Client()
  
 @client.event
 async def on_ready():
-    await client.change_presence(game=Game(name='trekvogels', type = 2))
+    await client.change_presence(game=Game(name='trekvogels'))
     print('trekvogel is ready :)')
    
 @client.event
@@ -29,5 +30,7 @@ async def on_message(message):
     if message.content.startswith('!vriend'):
         randomlist = ['20%','30%','40%','50%','60%','70%','80%','90%','100%']
         await client.send_message(message.channel,(random.choice(randomlist)))
-       
+    if message.content == '!test':
+        await client.send_message(message.channel,'test xD
+     
 client.run('NTYyNjc4Mzk3MTUxNjA4ODM0.XKTi8Q.eXADJ1QD76WrMds6EHcyU3CTtLs')
